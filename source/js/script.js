fa100b4dbd50400f7591cc1cad60f3baff284a84
Anton Tolstenko <restby@gmail.com>
2018-12-05 17:41:41 +0300

    начал js

---
+++ source/js/script.js
@@ -0,0 +1,45 @@
+var menuOpenBtn = document.querySelector('.navigation__btn');
+var menuCloseBtn = document.querySelector('.page-header__btn');
+
+var menu = document.querySelector('.navigation');
+
+// var exampleBtnPrev = document.querySelector('.example__toggle-btn--prev');
+// var exampleBtnNext = document.querySelector('.example__toggle-btn--next');
+// var exampleImgPrev = document.querySelector('.example__img--before');
+// var exampleImgNext = document.querySelector('.example__img--after');
+// var exampleToggle = document.querySelector('.example__toggle');
+
+// var mapPin = document.querySelector('.contacts__map-pin');
+// var mapStatic = document.querySelector('.contacts__map-img');
+// var mapInteractive = document.querySelector('.contacts__map-interactive');
+
+// mapPin.classList.remove('contacts__map-pin--nojs');
+// mapStatic.classList.remove('contacts__map-img--nojs');
+// mapInteractive.classList.remove('contacts__map-interactive--nojs');
+
+// menuOpenBtn.classList.remove('navigation__btn--nojs');
+// menuList.classList.remove('navigation__list--nojs');
+// menuCloseBtn.classList.remove('page-header__btn--nojs');
+
+// menuOpenBtn.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+//   menuList.classList.add('navigation__list--show');
+// });
+
+// exampleBtnPrev.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+//   if(!exampleImgPrev.classList.contains('example__img--show')) {
+//     exampleImgPrev.classList.add('example__img--show');
+//     exampleImgNext.classList.remove('example__img--show');
+//     exampleToggle.classList.remove('example__toggle--next')
+//   }
+// });
+
+// exampleBtnNext.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+//   if(!exampleImgNext.classList.contains('example__img--show')) {
+//     exampleImgNext.classList.add('example__img--show');
+//     exampleImgPrev.classList.remove('example__img--show');
+//     exampleToggle.classList.add('example__toggle--next')
+//   }
+// });
