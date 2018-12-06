9877c93df9322db3fc388dd8b1ec1142f486e3b5
Anton Tolstenko <restby@gmail.com>
2018-12-06 12:47:21 +0300

    все кроме проверки на ТЗ и критерии

--- source/js/script.js
+++ source/js/script.js
@@ -1,45 +1,25 @@
-var menuOpenBtn = document.querySelector('.navigation__btn');
-var menuCloseBtn = document.querySelector('.page-header__btn');
+var menuCloseBtn = document.querySelector('.navigation__btn');
+var menuOpenBtn = document.querySelector('.page-header__btn');
+var logoLink = document.querySelector('.page-header__link');
+var menuNav = document.querySelector('.navigation__list');
 
-var menu = document.querySelector('.navigation');
+menuOpenBtn.classList.remove('page-header__btn--no-js');
+menuOpenBtn.classList.add('page-header__btn--show');
+menuNav.classList.remove('navigation__list--no-js');
+logoLink.classList.add('page-header__link--move');
 
-// var exampleBtnPrev = document.querySelector('.example__toggle-btn--prev');
-// var exampleBtnNext = document.querySelector('.example__toggle-btn--next');
-// var exampleImgPrev = document.querySelector('.example__img--before');
-// var exampleImgNext = document.querySelector('.example__img--after');
-// var exampleToggle = document.querySelector('.example__toggle');
+menuOpenBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  menuOpenBtn.classList.remove('page-header__btn--show');
+  menuNav.classList.add('navigation__list--show');
+  logoLink.classList.remove('page-header__link--move');
+  menuCloseBtn.classList.remove('navigation__btn--no-js');
+});
 
-// var mapPin = document.querySelector('.contacts__map-pin');
-// var mapStatic = document.querySelector('.contacts__map-img');
-// var mapInteractive = document.querySelector('.contacts__map-interactive');
-
-// mapPin.classList.remove('contacts__map-pin--nojs');
-// mapStatic.classList.remove('contacts__map-img--nojs');
-// mapInteractive.classList.remove('contacts__map-interactive--nojs');
-
-// menuOpenBtn.classList.remove('navigation__btn--nojs');
-// menuList.classList.remove('navigation__list--nojs');
-// menuCloseBtn.classList.remove('page-header__btn--nojs');
-
-// menuOpenBtn.addEventListener('click', function(evt) {
-//   evt.preventDefault();
-//   menuList.classList.add('navigation__list--show');
-// });
-
-// exampleBtnPrev.addEventListener('click', function(evt) {
-//   evt.preventDefault();
-//   if(!exampleImgPrev.classList.contains('example__img--show')) {
-//     exampleImgPrev.classList.add('example__img--show');
-//     exampleImgNext.classList.remove('example__img--show');
-//     exampleToggle.classList.remove('example__toggle--next')
-//   }
-// });
-
-// exampleBtnNext.addEventListener('click', function(evt) {
-//   evt.preventDefault();
-//   if(!exampleImgNext.classList.contains('example__img--show')) {
-//     exampleImgNext.classList.add('example__img--show');
-//     exampleImgPrev.classList.remove('example__img--show');
-//     exampleToggle.classList.add('example__toggle--next')
-//   }
-// });
+menuCloseBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  menuOpenBtn.classList.add('page-header__btn--show');
+  menuNav.classList.remove('navigation__list--show');
+  logoLink.classList.add('page-header__link--move');
+  menuCloseBtn.classList.add('navigation__btn--no-js');
+});
